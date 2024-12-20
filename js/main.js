9c645c9fee6b02d92c3799c0941af0d2719fcf9a
emuche <ucheemmanuel1@gmail.com>
2024-12-20 08:37:23 +0100

    third  commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-$('.hover').mousemove(function (e) { 
+$(document).ready(function () {
+    $('body').prepend('<div id="hoverdiv"></div>');
+    $('#hoverdiv').css({'display': 'none', 'position': 'absolute', 'font-size': '14px', 'background-color': '#fff', 'color': '#404040', 'border': '1px solid #999', 'padding': '7px'});  
+});
+
+$('[hovertext]').mousemove(function (e) { 
     var hovertext = $(this).attr('hovertext');
     $('#hoverdiv').text(hovertext).show();
     $('#hoverdiv').css('top', e.clientY + 10).css('left', e.clientX + 10);
