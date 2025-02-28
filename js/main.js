cdbc02a5a416df2c3a4dc833a2c654c611a44a65
ecuche <ucheemmanuel1@gmail.com>
2025-02-28 22:00:13 +0100

    hovertext without jquery

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
-$(document).ready(function () {
-
-    $('body').append('<div id="hoverdiv"></div>');
-    $('#hoverdiv').css({'display':'none', 'position':'absolute', 'font-size': '14px', 'background-color': '#fff', 'color': '#404040', 'border': '1px solid #999', 'padding': '7px' });
-
-    $(document).on('mousemove', '[hovertext]', function(e){
-        var text = $(this).attr('hovertext');
-
-        $('#hoverdiv').text(text).show();
-        $('#hoverdiv').css({'top': e.clientY + 10, 'left': e.clientX + 10});
+document.addEventListener('DOMContentLoaded', () => {
+    var body = document.querySelector('body');
+    body.insertAdjacentHTML('afterbegin', '<div id="hoverdiv"></div>');
+    var hoverDiv = document.getElementById('hoverdiv');
+    hoverDiv.style.cssText = `display: none;`;
+    var hovertexts = document.querySelectorAll('[hovertext]');
+    hovertexts.forEach((hovertext)=>{
+        var value = hovertext.getAttribute('hovertext');
+        hovertext.addEventListener("mousemove", (e)=>{
+            hoverDiv.innerHTML = value;
+            hoverDiv.style.cssText = `display: block; position: fixed; font-weight: bold; font-size: 14px; font-family: Lucida Handwriting; background-color: #fff; color: #404040; border: 1px solid #999; padding: 7px`;
+            hoverDiv.style.top = e.clientY + 5 + 'px';
+            hoverDiv.style.left = e.clientX + 5 + 'px';
+        });
+        hovertext.addEventListener("mouseout", (e)=>{
+            hoverDiv.style.cssText = `display: none;`;
+        });
     });
-
-    $(document).on('mouseout', '[hovertext]', function(e){
-        $('#hoverdiv').hide();
-    });
-
 });
