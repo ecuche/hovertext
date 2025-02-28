faa61c8757e90972c4d5627e3745b7ffc3cec1ac
ecuche <ucheemmanuel1@gmail.com>
2025-02-28 22:25:41 +0100

    hovertext without jquery updated

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-    var body = document.querySelector('body');
-    body.insertAdjacentHTML('afterbegin', '<div id="hoverdiv"></div>');
-    var hoverDiv = document.getElementById('hoverdiv');
-    hoverDiv.style.cssText = `display: none;`;
     var hovertexts = document.querySelectorAll('[hovertext]');
-    hovertexts.forEach((hovertext)=>{
-        var value = hovertext.getAttribute('hovertext');
-        hovertext.addEventListener("mousemove", (e)=>{
-            hoverDiv.innerHTML = value;
-            hoverDiv.style.cssText = `display: block; position: fixed; font-weight: bold; font-size: 14px; font-family: Lucida Handwriting; background-color: #fff; color: #404040; border: 1px solid #999; padding: 7px`;
-            hoverDiv.style.top = e.clientY + 5 + 'px';
-            hoverDiv.style.left = e.clientX + 5 + 'px';
+    if(hovertexts.length > 0){
+        document.body.insertAdjacentHTML('beforeend', '<div style="display:none;"></div>');
+        var hoverDiv = document.querySelectorAll('div');
+        hoverDiv = hoverDiv[hoverDiv.length - 1];   
+        hovertexts.forEach((hovertext)=>{
+            var value = hovertext.getAttribute('hovertext');
+            hovertext.addEventListener("mousemove", (e)=>{
+                hoverDiv.innerHTML = value;
+                hoverDiv.style.cssText = `display: block; position: fixed; font-weight: bold; font-size: 14px; font-family: courier; background-color: #fff; color: #404040; border: 1px solid #999; padding: 7px`;
+                hoverDiv.style.top = e.clientY + 5 + 'px';
+                hoverDiv.style.left = e.clientX + 5 + 'px';
+            });
+            hovertext.addEventListener("mouseout", (e)=>{
+                hoverDiv.style.cssText = `display: none;`;
+            });
         });
-        hovertext.addEventListener("mouseout", (e)=>{
-            hoverDiv.style.cssText = `display: none;`;
-        });
-    });
+    }
 });
